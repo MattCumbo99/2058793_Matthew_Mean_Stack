becac6c213e8e107f5afc04abc760d162c1420df
student <matthew.cumbo@tcs.com>
2021-08-03 18:31:20 +0000

    Finishing touches

--- Phase 1/team-budget-planner/budget_functions.js
+++ Phase 1/team-budget-planner/budget_functions.js
@@ -2,6 +2,20 @@ function addData() {
     let client = document.getElementById("clname").value;
     let proj = document.getElementById("prname").value;
     let budget = document.getElementById("budget").value;
+
+    if (client.length == 0) {
+        alert("Client name cannot be empty.");
+        return;
+    }
+    if (proj.length == 0) {
+        alert("Project name cannot be empty.");
+        return;
+    }
+    if (budget.length == 0) {
+        alert("Budget amount cannot be empty.");
+        return;
+    }
+
     let companies = JSON.parse(localStorage.getItem("companies") || "[]");
     let company = {cl:client,pr:proj,bu:budget};
     
@@ -15,13 +29,19 @@ function displayData() {
     let companies = JSON.parse(localStorage.getItem("companies") || "[]");
     var tableContent = "";
     var tableStart = "<label>Annual Budget</label><br><br><table border=1><tr><th>Client Name</th><th>Project Name</th><th>Budget</th></tr>";
-    var tableEnd = "</table><br><a href=\"MainPage.html\">Back</a>";
+    var tableEnd = "</table>";
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+    let total = 0;
     
     companies.forEach(element => {
-        tableContent += "<tr><td>"+element.cl+"</td><td>"+element.pr+"</td><td>"+element.bu+"</td></tr>";
+        tableContent += "<tr><td>"+element.cl+"</td><td>"+element.pr+"</td><td>"+formatter.format(element.bu)+"</td></tr>";
+        total = +total + +element.bu;
     });
 
-    tableContent = tableStart + tableContent + tableEnd;
+    tableContent = tableStart + tableContent + tableEnd + "<br><label>Total cost: " + formatter.format(total) + "</label><br><br><a href=\"MainPage.html\">Back</a>";
     document.getElementById("main").innerHTML = tableContent;
 }
 

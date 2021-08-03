827ac0d90a5e778a5a2bbb129170a20950a9ee9f
student <matthew.cumbo@tcs.com>
2021-08-03 18:00:15 +0000

    Functional

--- Phase 1/team-budget-planner/budget_functions.js
+++ Phase 1/team-budget-planner/budget_functions.js
@@ -1,35 +1,31 @@
-var companies = [];
-
-class Company {
-    constructor(cl, pr, bu) {
-        this.client = cl;
-        this.project = pr;
-        this.budget = bu;
-    }
-
-    getClient() {
-        return this.client;
-    }
-    getProject() {
-        return this.project;
-    }
-    getBudget() {
-        return this.budget;
-    }
-}
-
 function addData() {
-    var client = document.getElementById("clname").value;
-    var proj = document.getElementById("prname").value;
-    var budget = document.getElementById("budget").value;
-
-    let cur = new Company(client, proj, budget);
-    companies.push(cur);
-    sessionStorage.setItem("companies", JSON.stringify(companies));
+    let client = document.getElementById("clname").value;
+    let proj = document.getElementById("prname").value;
+    let budget = document.getElementById("budget").value;
+    let companies = JSON.parse(localStorage.getItem("companies") || "[]");
+    let company = {cl:client,pr:proj,bu:budget};
+    
+    companies.push(company);
     localStorage.setItem("companies", JSON.stringify(companies));
-    alert("Successfully added " + cur.client);
+
+    alert("Successfully added " + company.cl);
 }
 
 function displayData() {
+    let companies = JSON.parse(localStorage.getItem("companies") || "[]");
+    var tableContent = "";
+    var tableStart = "<label>Annual Budget</label><br><br><table border=1><tr><th>Client Name</th><th>Project Name</th><th>Budget</th></tr>";
+    var tableEnd = "</table><br><a href=\"MainPage.html\">Back</a>";
+    
+    companies.forEach(element => {
+        tableContent += "<tr><td>"+element.cl+"</td><td>"+element.pr+"</td><td>"+element.bu+"</td></tr>";
+    });
 
+    tableContent = tableStart + tableContent + tableEnd;
+    document.getElementById("main").innerHTML = tableContent;
+}
+
+function removeData() {
+    localStorage.removeItem("companies");
+    alert("Cleared all data");
 }

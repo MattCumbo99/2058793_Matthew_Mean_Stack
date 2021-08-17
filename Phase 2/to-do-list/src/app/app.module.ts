2d885fc7e0019ed631ea3c36b9def9adac0df61d
Matthew Cumbo <mattcumbo99@gmail.com>
2021-08-17 15:05:48 -0500

    Finishing touches

--- Phase 2/to-do-list/src/app/app.module.ts
+++ Phase 2/to-do-list/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskTrackerComponent } from './task-tracker/task-tracker.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Phase 2/to-do-list/src/app/task-tracker/task-tracker.component.ts
+++ Phase 2/to-do-list/src/app/task-tracker/task-tracker.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+export interface TaskElement {
+  id:string;
+  name:string;
+  task:string;
+  deadline:string;
+}
 
 @Component({
   selector: 'app-task-tracker',
@@ -6,10 +14,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-tracker.component.css']
 })
 export class TaskTrackerComponent implements OnInit {
-
+  dataSource:TaskElement[] = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addTask(taskRef:NgForm): void {
+    let theTask = taskRef.value;
+    let nTask = {id:theTask.id, name:theTask.name, task:theTask.task, deadline:theTask.deadline};
+
+    this.dataSource.push(nTask);
+    taskRef.reset();
+    this.displayTable();
+  }
+
+  displayTable(): void {
+    let dataTable = document.getElementById("tasks") as HTMLElement;
+    let display:string = "";
+    let exists:boolean = false;
+
+    for (let task of this.dataSource) {
+      if (!exists) {
+        display += "<br><table border='1' style='margin: 0 auto;'><tr><th>ID</th><th>Name</th><th>Task</th><th>Deadline</th></tr>";
+        exists = true;
+      }
+      display += "<tr><td>" + task.id + "</td><td>" + task.name + "</td><td>" + task.task + "</td><td>" + task.deadline + "</td></tr>";
+    }
+    if (!exists) {
+      dataTable.innerHTML = "<h4>No tasks yet</h4>";
+    }
+    else {
+      dataTable.innerHTML = display + "</table>";
+    }
   }
 
 }

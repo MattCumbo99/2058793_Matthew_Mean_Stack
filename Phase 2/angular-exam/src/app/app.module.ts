020e1549dc65056da47f3d258c0cfb877a27c069
Matthew Cumbo <mattcumbo99@gmail.com>
2021-08-18 12:37:36 -0500

    Angular materials

--- Phase 2/angular-exam/src/app/app.module.ts
+++ Phase 2/angular-exam/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExamFormComponent } from './exam-form/exam-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ExamFormComponent } from './exam-form/exam-form.component';
     ExamFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

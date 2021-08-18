f93d3c7b160e56bedce5f6ceba8efe59639d7e93
Matthew Cumbo <mattcumbo99@gmail.com>
2021-08-18 16:19:59 -0500

    Question servicers

---
+++ Phase 2/angular-exam/src/app/question.model.ts
@@ -0,0 +1,10 @@
+export class Question {
+    constructor(
+        public title:string,
+        public ans1:string,
+        public ans2:string,
+        public ans3:string,
+        public ans4:string,
+        public correctAns:string
+    ) { }
+}

c58a768846335f8a8d1b9c8e5f14e40f0b4cfa8c
Matthew Cumbo <mattcumbo99@gmail.com>
2021-08-19 10:37:03 -0500

    Working total correct

--- Phase 2/angular-exam/src/app/exam-form/exam-form.component.ts
+++ Phase 2/angular-exam/src/app/exam-form/exam-form.component.ts
@@ -12,7 +12,9 @@ import { QuestionService } from '../questions.service';
 export class ExamFormComponent implements OnInit {
   examination:Array<Question> = [];
   curForm:FormGroup;
-  selectedAnswers:Array<Question> = [];
+  correctAnswers:Array<Question> = [];
+  totalCorrect:number = 0;
+  examDone:boolean = false;
 
   constructor(public form:FormBuilder, public questionSer:QuestionService) { 
     this.curForm = form.group({});
@@ -22,13 +24,14 @@ export class ExamFormComponent implements OnInit {
     this.loadQuestions();
   }
 
+  // Loads the question bank to the form
   loadQuestions(): void {
     this.questionSer.getQuestionBank().subscribe(result=> {
       for (let q of result) {
         this.curForm.addControl(q.title, this.form.control(''));
         this.examination.push(q);
       }
-    })
+    });
   }
 
   radioChange(event:MatRadioChange, data:any): void {
@@ -37,8 +40,20 @@ export class ExamFormComponent implements OnInit {
   }
 
   finishExam(examRef:NgForm): void {
-    this.curForm = examRef.form.value;
-    console.log(examRef.form.value); // Displays all answers
+    let spot:number = 0;
+    // Go through each question
+    Object.keys(examRef.value).forEach(key => {
+      //console.log(examRef.value[key]); // Get selected answer from key
+
+      // Check for correct answers
+      if (this.examination[spot].correctAns == examRef.value[key]) {
+        this.correctAnswers.push(this.examination[spot]); // Add correct answer to bank
+      }
+      spot++;
+    });
+    this.totalCorrect = this.correctAnswers.length;
+    this.examDone = true;
+
   }
 
 }

06f9f8e09527a37f2930a6fcaa8a5598afebd994
Matthew Cumbo <mattcumbo99@gmail.com>
2021-08-23 13:44:34 -0500

    Added date and time of storage

--- Phase 3/node-logging/logger.js
+++ Phase 3/node-logging/logger.js
@@ -5,26 +5,28 @@ var r1 = obj.createInterface({
     output:process.stdout
 });
 
-let lastCommand = "";
+//let lastCommand = "";
 
-do {
+//do {
     r1.question("Enter your first name: ", (fname)=> {
         r1.question("Enter your last name: ", (lname)=> {
             r1.question("Enter your gender: ", (genType)=> {
                 r1.question("Enter your email: ", (emailAddress)=> {
                     let people = JSON.parse(fs.readFileSync("logged-info.json").toString());
-                    let person = {firstname:fname, lastname:lname, gender:genType, email:emailAddress};
+                    let curDate = new Date();
+                    let person = {firstname:fname, lastname:lname, gender:genType, email:emailAddress,
+                                    dateStored:curDate.getDate(), timeStored:curDate.getTime()};
                     people.push(person);
                     fs.writeFileSync("logged-info.json",JSON.stringify(people));
                     console.log("Record logged successfully.");
-                    r1.question("Make another input? (y/n): ", (answer)=> {
-                        if (answer != "y" || answer != "Y") {
-                            answer = "n";
-                        }
-                        lastCommand = answer;
-                    })
+                    // r1.question("Make another input? (y/n): ", (answer)=> {
+                    //     if (answer != "y" || answer != "Y") {
+                    //         answer = "n";
+                    //     }
+                    //     lastCommand = answer;
+                    // })
                 });
             });
         });
     });
-} while (lastCommand!="y" || lastCommand!="Y");
+//} while (lastCommand!="y" || lastCommand!="Y");

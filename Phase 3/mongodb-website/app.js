223f42bf28dc589f8069e4bec937b05e11566be2
Matthew Cumbo <mattcumbo99@gmail.com>
2021-09-07 10:32:36 -0500

    Node JS functions

--- Phase 3/mongodb-website/app.js
+++ Phase 3/mongodb-website/app.js
@@ -1,9 +1,28 @@
-let mongoClient = require("mongodb").MongoClient;
+let express = require("express");
+let bodyParser = require("body-parser");
 
-let url = "mongodb://localhost:27017";
+let app = express();
 
-mongoClient.connect(url, (err,client)=> {
-    if (!err) {
-        
-    }
+app.use(bodyParser.json());
+
+app.get("/", (request,response)=> {
+    response.sendFile(__dirname+"\\Index.html");
 });
+
+app.get("/AddCourse", (request,response)=> {
+    response.sendFile(__dirname+"\\AddCourse.html");
+});
+
+app.get("/UpdateCourse", (request,response)=> {
+    response.sendFile(__dirname+"\\UpdateCourse.html");
+});
+
+app.get("/DeleteCourse", (request,response)=> {
+    response.sendFile(__dirname+"\\DeleteCourse.html");
+});
+
+app.get("/FetchCourse", (request,response)=> {
+    response.sendFile(__dirname+"\\FetchCourse.html");
+});
+
+app.listen(9090, ()=>console.log("Server running on port 9090"));
